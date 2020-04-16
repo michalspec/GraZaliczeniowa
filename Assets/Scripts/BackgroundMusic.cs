d36b137497b84d7e5d29b926deaa323906148b53
michalspec <50529391+michalspec@users.noreply.github.com>
2020-04-16 20:39:45 +0200

    ObstaclesModels and Audio

---
+++ Assets/Scripts/BackgroundMusic.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusic : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private static BackgroundMusic instance = null;
+    private static BackgroundMusic Instance
+    {
+        get { return instance; }
+    }
+    private void Awake()
+    {
+        if(instance != null && instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            instance = this;
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/StoneColumnColapse.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StoneColumnColapse : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform player;
+    public Transform column;
+    public Rigidbody rb;
+    public float distance = 50f;
+    private bool flag = true;
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+        if(column.position.z - player.position.z < distance && flag ==true)
+        {
+            rb.freezeRotation = false;
+            column.Rotate(0,0, 7,Space.World);
+            flag = false;
+        }
+    }
+}
